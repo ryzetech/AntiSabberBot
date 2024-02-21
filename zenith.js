7c9c0458a38f089ad0e1c3dc7a9499fa2bcb7233
Finnley <git@finnley.dev>
2024-02-21 14:05:45 +0100

    ✨ added evaluation

---
+++ zenith.js
@@ -0,0 +1,55 @@
+import fetch from "node-fetch"
+import { Context } from "telegraf";
+import FormData from "form-data"
+
+import filters from "./filters.js";
+
+/**
+ * 
+ * @param {Context} ctx 
+ * @returns {Array}
+ */
+async function evaluate(ctx) {
+  if (ctx.message.sticker.is_animated || ctx.message.sticker.is_video) return [{ error: "animated" }];
+
+  try {
+    let link = await ctx.telegram.getFileLink(ctx.message.sticker.file_id);
+
+    const sticker_res = await fetch(link);
+    const sticker_data = (await sticker_res.buffer());
+
+    const form = new FormData();
+
+    form.append("image", sticker_data, "image.png");
+
+    const upload_res = await fetch("https://pawgge.rs/api/image", {
+      method: "POST",
+      body: form,
+    });
+
+    const upload_data = await upload_res.json();
+
+    const result = await fetch(`https://pawgge.rs/api/result?file_md5=${upload_data.file_md5}`);
+    const enddata = await result.json();
+
+    return enddata.result;
+  } catch (e) {
+    console.log(e);
+    return [{ error: "other" }];
+  }
+}
+
+/**
+ * @returns {Boolean}
+ */
+async function isAllowed(evaluation) {
+  for (const tag in evaluation) {
+    if (filters.disallow.includes(tag)) { return false };
+  }
+  return true;
+}
+
+export default {
+  evaluate,
+  isAllowed
+}
